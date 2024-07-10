a0de44b31b2dbab9e539e76ac98b742d117605e6
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-07-10 16:54:52 -0700

    Commit

---
+++ CoreDatabaseSinger/SingerNameShowViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SingerNameShowViewController.swift
+//  CoreDatabaseSinger
+//
+//  Created by Arpit iOS Dev. on 10/07/24.
+//
+
+import UIKit
+
+class SingerNameShowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
